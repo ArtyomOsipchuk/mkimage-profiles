b99c1e137e2e792465f7edbdf5fbd784682bb109
Artyom Osipchuk <artos4002@yandex.ru>
2024-11-10 21:42:52 +0300

    "Protocols in Linux" feature fixed - conf.d/network-protocols-in-linux.mk modified a lot - now we can get "protocols"-image based on jeos

---
+++ features.in/02protocols/files/root/.zshenv
@@ -0,0 +1,5 @@
+# ~/.zshenv
+
+path=(/root/bin /sbin /usr/sbin /usr/local/sbin /bin /usr/bin /usr/local/bin)
+USERNAME="root"
+export USERNAME PATH
---
+++ features.in/02protocols/image-scripts.d/00-chmodder.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+chmod 600 /etc/sysconfig/rpcbind
+chmod 600 /etc/syslog.conf
+chmod 600 /etc/openssh/ssh_host_dsa_key
+chmod 600 /etc/openssh/ssh_host_ecdsa_key
+chmod 600 /etc/openssh/ssh_host_ed25519_key
+chmod 600 /etc/openssh/ssh_host_rsa_key
