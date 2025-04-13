ba8b32d0a3484dc195a34712a0dad16710894c36
ArtyomOsipchuk <artos4002@yandex.ru>
2025-04-14 01:21:16 +0300

    Добавлены пакеты в профиль протоколов. Добавлена новая фича unroot с более строгими правилами

---
+++ features.in/03unroot/image-scripts.d/15-no-live-usb-boot.sh
@@ -0,0 +1,7 @@
+# скорее всего понадобится протыкать это именно в m-p
+#sudo /etc/default/grub < """
+#GRUB_DISABLE_OS_PROBER=true  
+#GRUB_DISABLE_RECOVERY=true 
+#"""
+#sudo update-grub
+# может это слишком жестокие требования
