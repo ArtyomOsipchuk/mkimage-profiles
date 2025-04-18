e062069278c40f5b0393a9eb2feb454ec57ba5d7
ArtyomOsipchuk <artos4002@yandex.ru>
2025-04-18 17:03:01 +0300

    Initial for Профиль образа для кафедрального курса "Разработка программного обеспечения для GNU/Linux" АСВК

--- features.in/03unroot/image-scripts.d/15-no-live-usb-boot.sh
+++
@@ -1,7 +0,0 @@
-# скорее всего понадобится протыкать это именно в m-p
-#sudo /etc/default/grub < """
-#GRUB_DISABLE_OS_PROBER=true  
-#GRUB_DISABLE_RECOVERY=true 
-#"""
-#sudo update-grub
-# может это слишком жестокие требования
