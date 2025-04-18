0826f3aef045d338d3ef4089cbdf56117e8db85d
ArtyomOsipchuk <artos4002@yandex.ru>
2025-04-18 17:05:45 +0300

    Initial for image with old Nvidia drivers for asm course

--- conf.d/CUSTOMS.mk
+++ conf.d/CUSTOMS.mk
@@ -1,25 +1,7 @@
 # Пользовательские образы как цель этого гит-форка
+# Образ для компьютерного класса со старыми карточками NVidia
 
 # make nvidia-asm-mate.iso BRANCH=sisyphus
 distro/nvidia-asm-mate: distro/.regular-gtk mixin/regular-mate \
 	use/x11/3d use/stage2/kms/nvidia; @:
 	@$(call add,THE_LISTS,nvidia-asm-mate)
-
-distro/unroot-mate: distro/.regular-gtk mixin/regular-mate \
-	use/x11/3d use/stage2/kms/nvidia use/03unroot; @:
-	@$(call add,THE_LISTS,nvidia-asm-mate)
-
-# make ROOTPW=root vm/protocols-jeos.vdi BRANCH=sisyphus VM_SIZE=4294967296
-vm/protocols-jeos: vm/.base-grub use/init/systemd \
-	use/deflogin use/02protocols \
-	use/services/lvm2-disable \
-	use/tty/S0
-ifneq (,$(filter-out i586 x86_64,$(ARCH)))
-	@$(call add,DEFAULT_SERVICES_DISABLE,multipathd)
-endif
-	@$(call add,DEFAULT_SERVICES_ENABLE,sshd)
-	@$(call add,DEFAULT_SERVICES_ENABLE,sethostname)
-	@$(call add,DEFAULT_SERVICES_ENABLE,getty@tty1)
-	@$(call add,DEFAULT_SERVICES_DISABLE,avahi-daemon.service)
-	@$(call add,DEFAULT_SERVICES_DISABLE,avahi-daemon.socket)
-	@$(call add,THE_LISTS,network-protocols-in-linux)
--- features.in/02protocols/files/etc/profile.d/0lang.sh
+++
@@ -1,4 +0,0 @@
-# /etc/profile.d/0lang.sh - set i18n stuff
-LC_ALL=C
-LC_COLLATE=C
-LANG=C
