1d15f87cead8d8af07960d79f6ea0a1be5a7bf99
ArtyomOsipchuk <artos4002@yandex.ru>
2025-07-15 15:31:38 +0300

    Replace default greeting wallpaper with custom in XFCE

--- features.in/03unroot/live/image-scripts.d/99-background.sh
+++ features.in/03unroot/live/image-scripts.d/99-background.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+WALLPAPER_GREETING='/usr/share/backgrounds/vladstudio_celestial_cat_light_2048x1152.jpg'
 WALLPAPER_DESKTOP='/usr/share/backgrounds/vladstudio_emotions_2048x1152.jpg'
+
+echo "background=$WALLPAPER_GREETING" >> /etc/lightdm/lightdm-gtk-greeter.conf
+DEFAULT='/usr/share/backgrounds/xfce/default-background'
+
 rm -f "$DEFAULT"
 ln -s "$WALLPAPER_DESKTOP" "$DEFAULT"
