027fcb361f71eacf462a1af5f1b6a99940524b6d
Artyom Osipchuk <you@example.com>
2025-07-26 14:04:52 +0300

    DELETE

--- features.in/03unroot/live/image-scripts.d/99-background.sh
+++ features.in/03unroot/live/image-scripts.d/99-background.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-WALLPAPER_GREETING='/usr/share/backgrounds/vladstudio_celestial_cat_light_2048x1152.jpg'
-WALLPAPER_DESKTOP='/usr/share/backgrounds/vladstudio_emotions_2048x1152.jpg'
-
+WALLPAPER_GREETING='/usr/share/backgrounds/xfce/xfce-cp-dark.svg'
+WALLPAPER_DESKTOP='/usr/share/backgrounds/xfce/xfce-leaves.svg'
 echo "background=$WALLPAPER_GREETING" >> /etc/lightdm/lightdm-gtk-greeter.conf
 DEFAULT='/usr/share/backgrounds/xfce/default-background'
 
