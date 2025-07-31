ebe812f266416a2212882c5b07a20f014c0aff21
ArtyomOsipchuk <artos4002@yandex.ru>
2025-07-31 17:09:55 +0300

    Initial for ASVK prac

---
+++ conf.d/CUSTOMS.mk
@@ -0,0 +1,6 @@
+# Пользовательские образы как цель этого гит-форка
+# Образ для практикума по питону для сетевой загрузки в компьютерных классах
+
+# make prac-xfce.iso BRANCH=sisyphus
+distro/prac-xfce: distro/.regular-gtk mixin/regular-xfce \
+	use/03prac; @:
