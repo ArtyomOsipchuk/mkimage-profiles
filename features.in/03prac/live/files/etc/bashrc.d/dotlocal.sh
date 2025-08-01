84d11a68da794266ff26c84315ec33a6fca9d233
ArtyomOsipchuk <you@example.com>
2025-08-01 22:31:14 +0300

    Added $HOME/.local/bin to $PATH

---
+++ features.in/03prac/live/files/etc/bashrc.d/dotlocal.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+case "$PATH" in
+	*:$HOME/bin|*:$HOME/bin:*|$HOME/bin:*) ;;
+	*) PATH="$HOME/bin:$PATH" ;;
+esac
+
+case "$PATH" in
+	*:$HOME/.local/bin|*:$HOME/.local/bin:*|$HOME/.local/bin:*) ;;
+	*) PATH="$HOME/.local/bin:$PATH" ;;
+esac
