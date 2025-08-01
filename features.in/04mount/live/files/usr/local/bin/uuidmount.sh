58136c461760dc0c73f0ab4800faccdc8bc01c34
ArtyomOsipchuk <you@example.com>
2025-08-01 21:48:41 +0300

    Added feature to enable custom systemd service for mounting net filesystems or disks by uuid from /proc/cmdline params

---
+++ features.in/04mount/live/files/usr/local/bin/uuidmount.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Парсим /proc/cmdline и извлекаем mount=DEVICE1^MOUNT_POINT1;DEVICE2^MOUNT_POINT2
+# где два уровня разделителей ; между парами, ^ между устройством и точкой монтирования
+PUBLIC_MOUNT=$(grep -oP 'mount=\K[^ ]+' /proc/cmdline || echo "")
+
+if [ -n "$PUBLIC_MOUNT" ]; then
+	IFS=';' read -r -a pairs <<< "$PUBLIC_MOUNT"
+	for pair in ${pairs[@]}; do
+		IFS='^' read -r -a parts <<< "$pair"
+		if [ ${#parts[@]} -eq 4 ]; then
+			DEVICE="${parts[0]}"
+			MOUNT_POINT="${parts[1]}"
+			SUBDIR="${parts[2]}"
+			BIND_MOUNT_POINT="${parts[3]}"
+			mkdir -p "$MOUNT_POINT"
+			mount "$DEVICE" "$MOUNT_POINT"
+			mount --bind $MOUNT_POINT/$SUBDIR $BIND_MOUNT_POINT
+		elif [ ${#parts[@]} -eq 2 ]; then
+			DEVICE="${parts[0]}"
+			MOUNT_POINT="${parts[1]}"
+			mkdir -p "$MOUNT_POINT"
+			mount "$DEVICE" "$MOUNT_POINT"
+		fi
+	done
+fi
+exit 0
