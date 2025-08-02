c8453bb84f4a37dbd3ee8732c151340bb8ca7445
ArtyomOsipchuk <artos4002@yandex.ru>
2025-08-02 15:13:37 +0300

    Modified use/live/x11 feature, to kill autologin

--- conf.d/CUSTOMS.mk
+++ conf.d/CUSTOMS.mk
@@ -24,3 +24,4 @@ distro/prac-xfce: distro/.regular-gtk mixin/regular-xfce \
 
 # !!! на этой ветке удалена цель use/net/etcnet
 # !!! на этой ветке модифицирована цель use/net/nm
+# !!! на этой ветке у use/live/x11 удалена цель use/x11-autologin
