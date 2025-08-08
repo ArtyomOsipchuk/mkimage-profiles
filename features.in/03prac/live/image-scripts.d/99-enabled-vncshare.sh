79500c416e8a3db95d7d7b30bc785841b7225940
ArtyomOsipchuk <artos4002@yandex.ru>
2025-08-08 15:54:26 +0300

    VNC service deleted. Image-script instead

--- features.in/03prac/live/image-scripts.d/99-enabled-vncshare.sh
+++
@@ -1,1 +0,0 @@
-ln -s /etc/systemd/system/vncshare.service /etc/systemd/system/multi-user.target.wants/vncshare.service
---
+++ features.in/03prac/live/image-scripts.d/99-vncshare-desktop.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/lib/vncshare-xfce4-panel-default-vncshare.sh -i
