61a4f9a9025b78b81a3ed5f13fbc659882cb8778
Anton Midyukov <antohami@altlinux.org>
2025-10-10 14:50:14 +0300

    alt-platform-builder.mk: INSTALLER=centaurus for p10
    There are no plans to replace it in p10.

--- conf.d/alt-platform-builder.mk
+++ conf.d/alt-platform-builder.mk
@@ -22,7 +22,11 @@ distro/alt-platform-builder: \
 	use/stage2/rtc use/stage2/sbc use/stage2/scsi use/stage2/usb \
 	use/stage2/drm use/stage2/sbc use/tty
 	@$(call set,BRANDING,alt-platform-builder)
+ifeq (p10,$(BRANCH))
+	@$(call set,INSTALLER,centaurus)
+else
 	@$(call set,INSTALLER,builder)
+endif
 #	@$(call set,DOCS,alt-platform-builder)
 	@$(call set,DISABLE_LANG_MENU,1)
 	@$(call set,META_PUBLISHER,BaseALT Ltd)
