ArtyomOsipchuk/mkimage-profiles

commit 17f79c3aaecba497b3eacf2e546f896578175465

e2k: explicit amdgpu support in install2
Looks like this helps installers with 5.4.0-2.20 based kernel
to install fine on Elbrus 801-PC with Radeon RX 580.

features.in/e2k/config.mk
@@ -25,6 +25,7 @@ endif
 
 use/e2k/x11: use/e2k use/x11
 	@$(call add,THE_PACKAGES,xorg-server xinit)
+	@$(call add,INSTALL2_PACKAGES,xorg-drv-amdgpu lccrt-blobs)
 
 ifeq (,$(filter-out e2kv5,$(ARCH)))
 use/e2k/multiseat/full: use/e2k/multiseat/901/full; @:

commit 29cd6008a097938dbaa29dfa27fea003cbf8c2f3

e2k: lccrt is not blobs anymore
We're building it off http://gitflic.ru/project/e2khome
sources for quite some time already.

Fixes: 17f79c3aaecba497b3eacf2e546f896578175465

features.in/e2k/config.mk
@@ -35,8 +35,8 @@ endif
 
 use/e2k/x11: use/e2k use/x11
 	@$(call add,THE_PACKAGES,xorg-server xinit)
-	@$(call add,INSTALL2_PACKAGES,xorg-drv-amdgpu lccrt-blobs)
-	@$(call add,LIVE_PACKAGES,xorg-drv-amdgpu lccrt-blobs)
+	@$(call add,INSTALL2_PACKAGES,xorg-drv-amdgpu lccrt)
+	@$(call add,LIVE_PACKAGES,xorg-drv-amdgpu lccrt)
 
 ifeq (,$(filter-out e2kv6,$(ARCH)))
 use/e2k/multiseat/full:
29cd6008a097938dbaa29dfa27fea003cbf8c2f3
Michael Shigorin <mike@altlinux.org>
2025-11-11 21:53:36 +0300

    e2k: lccrt is not blobs anymore
    We're building it off http://gitflic.ru/project/e2khome
    sources for quite some time already.
    
    Fixes: 17f79c3aaecba497b3eacf2e546f896578175465

--- features.in/e2k/config.mk
+++ features.in/e2k/config.mk
@@ -35,8 +35,8 @@ endif
 
 use/e2k/x11: use/e2k use/x11
 	@$(call add,THE_PACKAGES,xorg-server xinit)
-	@$(call add,INSTALL2_PACKAGES,xorg-drv-amdgpu lccrt-blobs)
-	@$(call add,LIVE_PACKAGES,xorg-drv-amdgpu lccrt-blobs)
+	@$(call add,INSTALL2_PACKAGES,xorg-drv-amdgpu lccrt)
+	@$(call add,LIVE_PACKAGES,xorg-drv-amdgpu lccrt)
 
 ifeq (,$(filter-out e2kv6,$(ARCH)))
 use/e2k/multiseat/full:
