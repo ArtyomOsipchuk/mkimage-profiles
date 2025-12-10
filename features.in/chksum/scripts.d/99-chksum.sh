42cbb58cb87ba334804602f772c824c963f74c3d
Anton Midyukov <antohami@altlinux.org>
2025-12-10 20:32:52 +0300

    chksum: replace 'DEBUG=1' with '-v' option for checksumgen command
    Suggested-by: manowar@

--- features.in/chksum/scripts.d/99-chksum.sh
+++ features.in/chksum/scripts.d/99-chksum.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd "$WORKDIR"
-find ALTLinux/ -name '*.rpm' | DEBUG=1 checksumgen -n -l - -o chksum/main.chksum
+find ALTLinux/ -name '*.rpm' | checksumgen -v -n -l - -o chksum/main.chksum
