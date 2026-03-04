21ad1a5e67747976888f956021c528d489496101
Ivan A. Melnikov <iv@altlinux.org>
2026-03-04 16:33:40 +0400

    stage2: Avoid DRM modules in ISO initrd on loongarch64 and riscv64
    With our recent loongarch64 kernel images, huge initrds stopped
    working in some virtual environments. Among available options
    and compromises we choose to drop DRM modules from stage1 (the
    initrd image of ISOs).
    
    Note that this currently disables plymouth during the initial
    boot from ISO. Making it work again is possible by employing
    simpledrm driver which is compiled into our kernels. This is
    left for the future work.
    
    Signed-off-by: Ivan A. Melnikov <iv@altlinux.org>

--- features.in/stage2/config.mk
+++ features.in/stage2/config.mk
@@ -13,12 +13,20 @@ use/stage2:: use/initrd-propagator; @:
 endif
 
 # building blocks for propagator's module cove
-use/stage2/ata use/stage2/drm use/stage2/fs use/stage2/hid use/stage2/md \
+use/stage2/ata use/stage2/fs use/stage2/hid use/stage2/md \
 	use/stage2/mmc use/stage2/net use/stage2/net-cifs use/stage2/net-nfs \
 	use/stage2/pcmcia use/stage2/rtc use/stage2/scsi use/stage2/usb \
 	use/stage2/virtio: \
 	use/stage2/%: use/stage2
 	@$(call add,STAGE1_MODLISTS,stage2-$*)
+
+# loongarch64 and riscv64 should rely on simpledrm.ko, which
+# should be built into the kernel image
+use/stage2/drm: use/stage2
+ifneq (,$(filter-out loongarch64 riscv64,$(ARCH)))
+	@$(call add,STAGE1_MODLISTS,stage2-drm)
+endif
+	@:
 
 use/stage2/sbc: use/stage2
 ifeq (,$(filter-out aarch64,$(ARCH)))
