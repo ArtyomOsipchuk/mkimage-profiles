de436a24f20a58e32ca8a54f3ced4046a7ee3e8c
Michael Shigorin <mike@altlinux.org>
2026-03-17 17:37:37 +0300

    lib/arch.mk: initial grub_arch()
    The list of GRUB-supported architectures
    has clearly broke out of its home feature;
    let's bring it to this new generic storage
    of architecture lists for specific aspects.

--- lib/arch.mk
+++ lib/arch.mk
@@ -2,3 +2,7 @@
 ARCH_VIRT = i586 x86_64 ppc64le aarch64 loongarch64 riscv64 e2k%
 ARCH_NOVM = e2k e2kv4 e2kv5
 virt_arch = $(filter $(ARCH_VIRT),$(filter-out $(ARCH_NOVM),$(ARCH)))
+
+# ditto for grub
+ARCH_GRUB = i586 x86_64 aarch64 riscv64 loongarch64
+grub_arch = $(filter $(ARCH_GRUB),$(ARCH))
