5b2d782fafa31302c8419a7dd4c31347eb46f186
Ivan A. Melnikov <iv@altlinux.org>
2026-03-30 19:26:22 +0400

    kernel: Set default KFLAVOURS to 6.18 for sisyphus_riscv64
    Now riscv64 is ready to switch to 6.18 as a default kernel.

--- features.in/kernel/config.mk
+++ features.in/kernel/config.mk
@@ -4,9 +4,6 @@ use/kernel:
 ifeq (,$(filter-out e2k%,$(ARCH)))
 	@$(call try,KFLAVOURS,elbrus-def)
 else
-ifeq (,$(filter-out riscv64,$(ARCH)))
-	@$(call try,KFLAVOURS,un-def)
-else
 ifeq (,$(filter-out sisyphus,$(BRANCH)))
 	@$(call try,KFLAVOURS,6.18)
 else
@@ -14,7 +11,6 @@ ifeq (,$(filter-out p11 c11% c10%,$(BRANCH)))
 	@$(call try,KFLAVOURS,6.12)
 else
 	@$(call try,KFLAVOURS,un-def)
-endif
 endif
 endif
 endif
