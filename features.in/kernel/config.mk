04be69d46857fc10bc18173f7e5aa117407406e2
Ivan A. Melnikov <iv@altlinux.org>
2026-03-30 19:25:04 +0400

    Revert "kernel: set default KFLAVOURS to 6.12 for ARCH=loongarch64"
    This reverts commit 50e657f505539f018f7b71e9b2d4140dca08c0d8.
    
    Now loongarch64 is ready to switch to 6.18 kernel for Sisyphus
    images by default.

--- features.in/kernel/config.mk
+++ features.in/kernel/config.mk
@@ -7,9 +7,6 @@ else
 ifeq (,$(filter-out riscv64,$(ARCH)))
 	@$(call try,KFLAVOURS,un-def)
 else
-ifeq (,$(filter-out loongarch64,$(ARCH)))
-	@$(call try,KFLAVOURS,6.12)
-else
 ifeq (,$(filter-out sisyphus,$(BRANCH)))
 	@$(call try,KFLAVOURS,6.18)
 else
@@ -17,7 +14,6 @@ ifeq (,$(filter-out p11 c11% c10%,$(BRANCH)))
 	@$(call try,KFLAVOURS,6.12)
 else
 	@$(call try,KFLAVOURS,un-def)
-endif
 endif
 endif
 endif
